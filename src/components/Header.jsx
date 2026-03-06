6720562b5ca026e7cd21404e607cf92154571100
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-03-06 23:09:46 +0200

    refactor: dynamic date and nickname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
-import "../styles/Header.css"
+import { useAuth } from "../contexts/AuthContext";
+import "../styles/Header.css";
 
 export const Header = () => {
-    return (
-        <header className="app-header">
-            <h1 className="welcome-msg">Hello, Dilara</h1>
-            <p className="current-date">Sunday, May 25</p>
-        </header>
-    )
-}
+  const { session } = useAuth();
+  const nickname =
+    session?.user.user_metadata.full_name ||
+    session?.user.user_metadata.name ||
+    session?.user.email.split("@gmail.com")[0];
+
+  const date = new Date();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+
+  return (
+    <header className="app-header">
+      <h1 className="welcome-msg">Hello, {nickname}</h1>
+      <p className="current-date">🗓️ {formattedDate}</p>
+    </header>
+  );
+};
