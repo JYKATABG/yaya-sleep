0c0f72e8a2d5ad18662327f84b63c694dea8e85f
Jykata <jykatabg@gmail.com>
2026-03-17 19:46:22 +0200

    refactor: app refactor components and make black text in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
-import "./App.css";
 import { Header } from "./components/Header";
 import { HistoryLogs } from "./components/HistoryLogs";
 import { SleepForm } from "./components/SleepForm";
 import { Stats } from "./components/Stats";
+import { AppShell, Container, Group, Stack } from "@mantine/core";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Stats />
-      <SleepForm />
-      <HistoryLogs />
-    </div>
+    <AppShell header={{ height: 70 }} padding="md" bg="#39c9bb">
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+
+      <AppShell.Main style={{
+      }}>
+        <Container size="lg">
+          <Stack gap="xl">
+            <Group
+              justify="space-between"
+              align="center"
+              mt="5em"
+              grow
+            >
+              <div style={{ flex: 1, maxWidth: '500px' }}>
+                <Stats />
+              </div>
+              <div style={{ flex: 1, maxWidth: '500px' }}>
+                <SleepForm />
+              </div>
+            </Group>
+            <HistoryLogs />
+          </Stack>
+        </Container>
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
