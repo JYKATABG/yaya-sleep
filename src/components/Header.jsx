a5b3e0794cacc7e5c98921113e9a292560b73b4b
Jykata <jykatabg@gmail.com>
2026-03-17 14:41:13 +0200

    refactor: header with mantine components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { useAuth } from "../contexts/AuthContext";
+import { Group, Text, Avatar, Menu, UnstyledButton, rem, Stack, Container } from '@mantine/core';
+import { IconLogout, IconChevronDown, IconCalendarStats, IconUser } from '@tabler/icons-react';
 import "../styles/Header.css";
 
 export const Header = () => {
@@ -16,14 +18,59 @@ export const Header = () => {
   }).format(date);
 
   return (
-    <header className="app-header">
-      <h1 className="welcome-msg">Hello, {nickname}</h1>
-      {session && (
-        <button className="logout-btn" onClick={signOut}>
-          Logout
-        </button>
-      )}
-      <p className="current-date">🗓️ {formattedDate}</p>
-    </header>
+    <Container fluid h={70} px="md" w="95%">
+      <Group justify="space-between" h="100%">
+
+        <Stack gap={0}>
+          <Text size="lg" fw={700} c="white">
+            {nickname}! 👋
+          </Text>
+          <Group gap={5}>
+            <IconCalendarStats size={14} color="white" />
+            <Text size="sm" c="white" fw={500}>
+              {formattedDate}
+            </Text>
+          </Group>
+        </Stack>
+
+        {session && (
+          <Menu shadow="md" width={200} position="bottom-end" transitionProps={{ transition: 'pop-top-right' }}>
+            <Menu.Target>
+              <UnstyledButton style={{ padding: '5px', borderRadius: '8px' }}>
+                <Group gap={7}>
+                  <Avatar
+                    src={session?.user.user_metadata.avatar_url}
+                    radius="xl"
+                    color="blue"
+                    variant="light"
+                  >
+                    {nickname?.charAt(0).toUpperCase()}
+                  </Avatar>
+                  <IconChevronDown size={rem(14)} stroke={1.5} />
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Application</Menu.Label>
+              <Menu.Item leftSection={<IconUser size={14} stroke={1.5} />}>
+                Profile
+              </Menu.Item>
+
+              <Menu.Divider />
+
+              <Menu.Label>Danger zone</Menu.Label>
+              <Menu.Item
+                color="red"
+                onClick={signOut}
+                leftSection={<IconLogout size={14} stroke={1.5} />}
+              >
+                Logout
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        )}
+      </Group>
+    </Container>
   );
 };
