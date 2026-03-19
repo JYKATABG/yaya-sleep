44ccdd32c5260b49260decaf24a2a07795b89347
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-03-19 15:49:47 +0200

    style: change menu arrow color to white

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ export const Header = () => {
         {session && (
           <Menu shadow="md" width={200} position="bottom-end" transitionProps={{ transition: 'pop-top-right' }}>
             <Menu.Target>
-              <UnstyledButton style={{ padding: '5px', borderRadius: '8px' }}>
+              <UnstyledButton style={{ padding: '5px', borderRadius: '8px', color: "white" }}>
                 <Group gap={7}>
                   <Avatar
                     src={session?.user.user_metadata.avatar_url}
