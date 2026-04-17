7f0b5908e55263f4b19b80d5a696058cfaa07707
stu2401261078 <stu2401261078@uni-plovdiv.bg>
2026-04-18 00:29:47 +0300

    feat: pwa setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Yaya Sleep",
+        short_name: "YayaSleep",
+        description: "Track your sleep and improve your health",
+        theme_color: "#39c9bb",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
